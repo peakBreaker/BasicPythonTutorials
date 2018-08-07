c714dc33e5470f4db1027acc01b26c89a601f2ca
Anders L. Hurum <andershurum@gmail.com>
2018-08-07 23:37:56 +0200

    added short tutorial on conditions

---
+++ tut_2/app.py
@@ -0,0 +1,21 @@
+# basic conditions
+
+myvar=4
+
+if myvar == 4:
+    print("myvar is 4!")
+
+if myvar < 10:
+    print("myvar is smaller than 10")
+
+# Can also use > (bigger than)
+
+if myvar < 4:
+    print("myvar is smaller than 4!")
+else:
+    print("myvar is bigger than 4!")
+
+if myvar >= 4:
+    print("myvar is bigger than or equal to 4!")
+else:
+    print("myvar is smaller than 4")
